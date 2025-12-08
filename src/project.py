b6b7539b918fdfbc8a21c6a33231ef80b7f7a851
Evelyn Aviles <evelynaviles900@gmail.com>
2025-12-08 17:20:45 -0600

    Enabled overrides on control shape

--- src/project.py
+++ src/project.py
@@ -4,6 +4,7 @@
 def left_color():
     for con in cmds.ls('L_*_CON'):
         shapes = cmds.listRelatives(con, s=True)
+        cmds.setAttr(shapes[0] + '.overrideEnabled', 1)
 #Take right controls and color them
 
 #Take middle controls and color them
