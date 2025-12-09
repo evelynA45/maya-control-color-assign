24767e9c6fdcb0f94dd2b40aa7c0407389d25cfa
Evelyn Aviles <evelynaviles900@gmail.com>
2025-12-08 18:11:03 -0600

    Adjusted primary function to adhere to right and middle controls

--- src/project.py
+++ src/project.py
@@ -7,5 +7,14 @@ def left_color():
         cmds.setAttr(shapes[0] + '.overrideEnabled', 1)
         cmds.setAttr(shapes[0] + '.overrideColor', 6)
 #Take right controls and color them
-
+def right_color():
+    for con in cmds.ls('R_*_CON'):
+        shapes = cmds.listRelatives(con, s=True)
+        cmds.setAttr(shapes[0] + '.overrideEnabled', 1)
+        cmds.setAttr(shapes[0] + '.overrideColor', 13)
 #Take middle controls and color them
+def middle_color():
+    for con in cmds.ls('*_CON'):
+        shapes = cmds.listRelatives(con, s=True)
+        cmds.setAttr(shapes[0] + '.overrideEnabled', 1)
+        cmds.setAttr(shapes[0] + '.overrideColor', 17)
